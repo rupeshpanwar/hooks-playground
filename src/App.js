f3dde1416326504a62c6ac5f0a89e1619fbe64c6
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-07-01 12:59:41 +0530

    use State with count

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import React, { useState } from 'react'
 
-
+//increament test for useState
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  const [activated, setActivated] = useState(false)
+  // const handleClick = () => setCount(count + 1) //method 1
 
-  const buttonText = activated ? 'activated' : 'deactivated'
-
-  function onClick() {
-    setActivated(!activated)
-  }
+  //method 2
+  const handleClick = () => setCount((prevCount) => {
+    return prevCount + 1;
+  })
 
   return (
-    <button onClick={onClick}>
-      {buttonText}
-    </button>
-  );
+    <>
+      <button onClick={handleClick}>
+        Increase
+      </button>
+      <h1>{count}</h1>
+    </>
+  )
 }
+
+//invoke useState
+// function App() {
+
+//   const [activated, setActivated] = useState(false)
+
+//   const buttonText = activated ? 'activated' : 'deactivated'
+
+//   function onClick() {
+//     setActivated(!activated)
+//   }
+
+//   return (
+//     <button onClick={onClick}>
+//       {buttonText}
+//     </button>
+//   );
+// }
+
 
 export default App;
