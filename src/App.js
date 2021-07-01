36e3c0671feb514c03e31f2f7fd30ff808f55f91
Rupesh Panwar <E020239@adgroup.michelin.com>
2021-07-01 10:41:38 +0530

    stateless function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Box(props){
+  return(
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  )
+}
+
 function App() {
   return (
-   <h1>Welcome to react-Hooks course!</h1>
+   <Box
+     name="Rupesh"
+     subtitle= "Panwar"
+   />
   );
 }
 
